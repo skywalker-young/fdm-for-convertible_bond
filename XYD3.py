2b13cc262075d14fa89af2319e32b60c60dd058c
膜法师 <30488918+skywalker-young@users.noreply.github.com>
2018-04-22 22:59:08 +0800

    Update XYD3.py

--- XYD3.py
+++ XYD3.py
@@ -12,3 +12,23 @@ for i in range(len(daily_returnSC)-window+1):
 
 plt.plot(macd,macdForSc)
 plt.show()
+############################
+
+这部分是研究波动率关系
+########################
+for i in range(move):
+   tmp= (daily_returnXYD[i*window:(i+1)*window])
+   tmp=sp.sqrt(sp.var(tmp))
+   macdForXYD.append(tmp)
+for i in range(move):
+     tmp = (daily_returnSC[i*window:(i + 1)*window])
+     tmp = sp.sqrt(sp.var(tmp))
+     macdForSC.append(tmp)
+
+plt.scatter(macdForXYD,macdForSC)
+f=interp1d(macdForXYD,macdForSC)
+xnew=np.linspace(min(macdForXYD),max(macdForXYD),num=500)
+plt.plot(macdForXYD,macdForSC,'o',xnew,f(xnew),'-')
+plt.xlabel("15days volatility of XYD")
+plt.ylabel("15days volatility of SC")
+plt.show()
